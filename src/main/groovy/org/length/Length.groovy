546ac3ba34a125171c8b9c6d3550aa05f929e49f
fistach <fistach@gmail.com>
2024-12-24 15:27:09 +0100

    add initialization with CM, M and MM

--- src/main/groovy/org/length/Length.groovy
+++ src/main/groovy/org/length/Length.groovy
@@ -6,8 +6,27 @@ class Length {
 
     Length(int value, Unit unit) {
         if (unit == Unit.MM) {
-            lengthInMillimeters = value
+            lengthInMillimeters =  value
+        } else if (unit == Unit.M) {
+            lengthInMillimeters = value * 1000;
+        } else if (unit == Unit.CM) {
+            lengthInMillimeters = value * 10
         }
+    }
+
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (o == null || getClass() != o.class) return false
+
+        Length length = (Length) o
+
+        if (lengthInMillimeters != length.lengthInMillimeters) return false
+
+        return true
+    }
+
+    int hashCode() {
+        return lengthInMillimeters
     }
 
     enum Unit {
