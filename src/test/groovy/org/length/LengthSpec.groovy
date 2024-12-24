a7b0ba31e65df3856c61d91d1f280819cab8f2c3
fistach <fistach@gmail.com>
2024-12-24 15:38:28 +0100

    add more tests

--- src/test/groovy/org/length/LengthSpec.groovy
+++ src/test/groovy/org/length/LengthSpec.groovy
@@ -21,4 +21,27 @@ to length initialized with meters'() {
         1000  | MM
     }
 
+    def 'length should be non-negative'() {
+        when:
+        new Length(-10, CM)
+
+        then:
+        def exception = thrown(IllegalStateException)
+        exception.message == "Length cannot be negative"
+    }
+
+    def 'the same length initialized in different units should be equal'() {
+        expect:
+        new Length(value1, unit1) == new Length(value2, unit2)
+
+        where:
+        value1 | unit1 | value2 | unit2
+        123    | CM    | 1230   | MM
+        983000 | MM    | 983    | M
+        983000 | MM    | 98300  | CM
+        45     | M     | 4500   | CM
+        567    | M     | 567000 | MM
+    }
+
+
 }
